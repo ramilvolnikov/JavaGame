0edb13614e9971e486142b55951184b0def9f828
maxsson <maxsson9@gmail.com>
2019-07-15 13:38:23 +0300

    Just check

--- app/src/main/java/com/example/sedecimannon/MainActivity.java
+++ app/src/main/java/com/example/sedecimannon/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    Integer a = 5;
+    Integer a = 6;
+    int b = a;
 }
