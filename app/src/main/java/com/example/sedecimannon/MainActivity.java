ad01268bd1089bd275e211b405f9c249cc8a7fb1
maxsson <maxsson9@gmail.com>
2019-07-16 11:18:29 +0300

    Added DecToBin(Integer)

--- app/src/main/java/com/example/sedecimannon/MainActivity.java
+++ app/src/main/java/com/example/sedecimannon/MainActivity.java
@@ -3,26 +3,42 @@ package com.example.sedecimannon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try{
+            String bin10 = DecToBin(10);
+            String bin0 = DecToBin(0);
+            String binm1 = DecToBin(-1);
+        }
+        catch (ArithmeticException e){Log.e("ERROR", String.valueOf(e));}
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    Integer a = 6;
-    int b = a;
 
-//function xuyanction
-    public static int bigger() {
-        int k = 0;
-        for (int i = 0; i<4; i++){
-            int n = (int) (Math.random() * 10);
-            if (n > k) {
-                k = n;
+//Decimal to binary system
+//TODO: rewrite like toBinaryString
+    protected String DecToBin (Integer dec){
+        if (dec == 0) return "0";
+        if (dec < 0) Log.e("ERROR", "Number less than zero");
+        String bin = "";
+        try{
+            while ( dec > 0){
+                bin += Integer.toString(dec % 2);
+                //dec /= 0;
+                dec/=2;
             }
-        }
-        return k;
+        }catch(ArithmeticException e){Log.e("ERROR", String.valueOf(e));}
+        Log.d("DecToBin", new StringBuilder(bin).reverse().toString());
+        return  new StringBuilder(bin).reverse().toString();
+    }
+
+//TODO:
+    protected String DecToBin (String dec){
+        return "-1";
     }
 }
