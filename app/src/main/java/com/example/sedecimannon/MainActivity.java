462c5b3896224c26c9bead6bf90977e2b8a87444
maxsson <maxsson9@gmail.com>
2019-08-06 15:58:42 +0300

    Merge branch 'master' of C:\Users\Administrator\AndroidStudioProjects\Sedecimannon with conflicts.

--- app/src/main/java/com/example/sedecimannon/MainActivity.java
+++ app/src/main/java/com/example/sedecimannon/MainActivity.java
@@ -2,23 +2,15 @@ package com.example.sedecimannon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     private TextView testTextView;
     private TextView TextView2;
-    private Button b1;
-    private Button b2;
-    private Button b3;
-    private Button b4;
-    private Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         try{
@@ -28,20 +20,6 @@ public class MainActivity extends Activity {
             String sbin10 = DecToBin("8");
             String sbin0 = DecToBin("0");
             String sbinm1 = DecToBin("-1");
-
-            String octal10 = DecToOctal(8);
-            String octal0 = DecToOctal(0);
-            String octalm1 = DecToOctal(-1);
-            String soctal10 = DecToOctal("8");
-            String soctal0 = DecToOctal("0");
-            String soctalm1 = DecToOctal("-1");
-
-            String hex10 = DecToHex(8);
-            String hex0 = DecToHex(0);
-            String hexm1 = DecToHex(-1);
-            String shex10 = DecToHex("8");
-            String shex0 = DecToHex("0");
-            String shexm1 = DecToHex("-1");
         }
         catch (ArithmeticException e){Log.e("ERROR", String.valueOf(e));}
         super.onCreate(savedInstanceState);
@@ -71,8 +49,7 @@ public class MainActivity extends Activity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                     ExprtoText(Integer.toString((int) (Math.random() * 50 - 10)));
-
+                        ExprtoText(Integer.toString((int) (Math.random() * 50 - 10)));
                     }
                 });
                 try {
@@ -92,22 +69,6 @@ public class MainActivity extends Activity {
         TextView2 = (TextView) findViewById(R.id.textView2);
         Integer textInTV2 = Integer.parseInt(TextView2.getText().toString());
         TextView2.setText(Integer.toString(textInTV2 + 1));
-        String d;
-
-        d = (DecToBin(s));
-        b1 = (Button) findViewById(R.id.button1);
-        b1.setTag(1);
-        b1.setText(d);
-
-        d = (DecToOctal(s));
-        b2 = (Button) findViewById(R.id.button2);
-        b2.setTag(1);
-        b2.setText(d);
-
-        d = (DecToHex(s));
-        b3 = (Button) findViewById(R.id.button3);
-        b3.setTag(1);
-        b3.setText(d);
     }
 
 //Decimal to binary system
@@ -126,55 +87,8 @@ public class MainActivity extends Activity {
         return  new StringBuilder(bin).reverse().toString();
     }
 
-    private String DecToOctal (Integer dec){
-        if (dec == 0) return "0";
-        if (dec < 0) Log.e("ERROR", "Number less than zero");
-        String octal = "";
-        try{
-            while ( dec > 0){
-                octal += Integer.toString(dec % 8);
-                dec /= 8;
-            }
-        }catch(ArithmeticException e){Log.e("ERROR", String.valueOf(e));}
-        Log.d("DecToOctal", new StringBuilder(octal).reverse().toString() + " number: " + dec);
-        return  new StringBuilder(octal).reverse().toString();
-    }
-
-    private String DecToHex (Integer dec){
-        if (dec == 0) return "0";
-        if (dec < 0) Log.e("ERROR", "Number less than zero");
-        String hex = "";
-        try{
-            while ( dec > 0){
-                hex += Integer.toString(dec % 16);
-                if ((new Integer(hex))>9) {
-                    hex = strhex[Integer.parseInt(hex)];
-                }
-                dec /= 16;
-            }
-        }catch(ArithmeticException e){Log.e("ERROR", String.valueOf(e));}
-        Log.d("DecToHex", new StringBuilder(hex).reverse().toString() + " number: " + dec);
-        return  new StringBuilder(hex).reverse().toString();
-    }
-
-    //TODO:
-
-    public String[] strhex = new String[16];{
-        strhex[10] = "A";
-        strhex[11] = "B";
-        strhex[12] = "C";
-        strhex[13] = "D";
-        strhex[14] = "E";
-        strhex[15] = "F";
-    }
+//TODO:
     private String DecToBin (String dec){
         return "-1";
     }
-    private String DecToOctal (String dec){
-        return "-1";
-    }
-    private String DecToHex (String dec){
-        return "-1";
-    }
-
 }
