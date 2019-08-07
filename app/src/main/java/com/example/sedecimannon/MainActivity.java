1f9b6fd096ea606543cafe06fa60e955fcc119fc
maxsson <maxsson9@gmail.com>
2019-08-07 12:25:09 +0300

    Check buttons. Adds todo's.

--- app/src/main/java/com/example/sedecimannon/MainActivity.java
+++ app/src/main/java/com/example/sedecimannon/MainActivity.java
@@ -2,26 +2,24 @@ package com.example.sedecimannon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private TextView testTextView;
     private TextView TextView2;
+    private Button b1;
+    private Button b2;
+    private Button b3;
+    private Button b4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        try{
-            String bin10 = DecToBin(8);
-            String bin0 = DecToBin(0);
-            String binm1 = DecToBin(-1);
-            String sbin10 = DecToBin("8");
-            String sbin0 = DecToBin("0");
-            String sbinm1 = DecToBin("-1");
-        }
-        catch (ArithmeticException e){Log.e("ERROR", String.valueOf(e));}
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -44,16 +42,16 @@ public class MainActivity extends AppCompatActivity {
     Runnable runnable = new Runnable() {
         @Override
         public void run() {
-            // имитация длительного процесса
+            // Endless cycle
             for (; ; ) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        ExprtoText(Integer.toString((int) (Math.random() * 50 - 10)));
+                     ExprtoText(Integer.toString((int) (Math.random() * 50 - 10)));
                     }
                 });
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(5000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -64,12 +62,30 @@ public class MainActivity extends AppCompatActivity {
 //Expression generation and text assignment
     private void ExprtoText(String s){
         testTextView = (TextView) findViewById(R.id.textView);
-        testTextView.setTag(1);
+        //testTextView.setTag(1);
         testTextView.setText(s);
         TextView2 = (TextView) findViewById(R.id.textView2);
         Integer textInTV2 = Integer.parseInt(TextView2.getText().toString());
         TextView2.setText(Integer.toString(textInTV2 + 1));
+        String d;
+        Integer is = Integer.parseInt(s);;
+        d = (DecToBin(is));
+        b1 = (Button)findViewById(R.id.button1);
+        //b1.setTag(1);
+        b1.setText(d);
+
+        d = (DecToOctal(is));
+        Button b2 = (Button)findViewById(R.id.button2);
+        //b2.setTag(1);
+        b2.setText(d);
+
+        // Do first good DecToHex
+        //d = (DecToHex(is));
+        //Button b3 = (Button)findViewById(R.id.button3);
+        //b3.setTag(1);
+        //b3.setText(d);
     }
+// TODO: Add description
 
 //Decimal to binary system
 // TODO: rewrite like toBinaryString
@@ -87,8 +103,58 @@ public class MainActivity extends AppCompatActivity {
         return  new StringBuilder(bin).reverse().toString();
     }
 
-//TODO:
+    private String DecToOctal (Integer dec){
+        if (dec == 0) return "0";
+        if (dec < 0) Log.e("ERROR", "Number less than zero");
+        String octal = "";
+        try{
+            while ( dec > 0){
+                octal += Integer.toString(dec % 8);
+                dec /= 8;
+            }
+        }catch(ArithmeticException e){Log.e("ERROR", String.valueOf(e));}
+        Log.d("DecToOctal", new StringBuilder(octal).reverse().toString() + " number: " + dec);
+        return  new StringBuilder(octal).reverse().toString();
+    }
+    // TODO: REWRITE!!!!
+    private String DecToHex (Integer dec){
+        if (dec == 0) return "0";
+        if (dec < 0) Log.e("ERROR", "Number less than zero");
+        String hex = "";
+        try{
+            while ( dec > 0){
+                hex += Integer.toString(dec % 16);
+                if ((new Integer(hex))>9) {
+                    hex = strhex[Integer.parseInt(hex)];
+                }
+                dec /= 16;
+            }
+        }catch(ArithmeticException e){Log.e("ERROR", String.valueOf(e));}
+        Log.d("DecToHex", new StringBuilder(hex).reverse().toString() + " number: " + dec);
+        return  new StringBuilder(hex).reverse().toString();
+    }
+
+
+
+    public String[] strhex = new String[16];{
+        strhex[10] = "A";
+        strhex[11] = "B";
+        strhex[12] = "C";
+        strhex[13] = "D";
+        strhex[14] = "E";
+        strhex[15] = "F";
+    }
+
+
+    //TODO: DO IT!!!!!!!!!!!!!
     private String DecToBin (String dec){
         return "-1";
     }
+    private String DecToOctal (String dec){
+        return "-1";
+    }
+    private String DecToHex (String dec){
+        return "-1";
+    }
+
 }
